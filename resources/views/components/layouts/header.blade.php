6c598940020587892167189bc5d10cc188582720
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2025-04-20 21:42:34 +0700

    fix(user-header): fix user header for now

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,46 +1,41 @@
 <header class=" bg-slate-300">
     <nav class="flex items-center justify-between px-12 py-4 mx-auto ">
-        <div class="flex items-center gap-4 ">
-            <a href="{{ route('home') }}"
-                class="font-medium cursor-pointer hover:underline underline-offset-4">
-                Home
-            </a>
-            <a href="{{ route('criterias.index') }}"
-                class="font-medium cursor-pointer hover:underline underline-offset-4">
-                Kriteria
-            </a>
-            <a href="{{ route('college-majors.index') }}"
-                class="font-medium cursor-pointer hover:underline underline-offset-4">
-                Jurusan Kuliah
-            </a>
-            <a href="{{ route('home') }}"
-                class="font-medium cursor-pointer hover:underline underline-offset-4">
-                Kriteria Jurusan
-            </a>
-            <a href="{{ route('student-scores.index') }}"
-                class="font-medium cursor-pointer hover:underline underline-offset-4">
-                Kalkulasi nilai
-            </a>
+        <a href="{{ route('home') }}" class=" bold text-2xl">Logo dan nama
+            app</a>
+        <div class=" flex items-center gap-8">
+            <div class=" flex gap-4 items-center">
+                <a href="#"
+                    class="font-medium cursor-pointer hover:underline underline-offset-4">
+                    About Us
+                </a>
+                <a href="#"
+                    class="font-medium cursor-pointer hover:underline underline-offset-4">
+                    FAQ
+                </a>
+                <a href="#"
+                    class="font-medium cursor-pointer hover:underline underline-offset-4">
+                    Contact
+                </a>
+            </div>
+            @auth
+                <div class="flex items-center gap-2">
+                    <a href="{{ route('profile') }}"
+                        class="font-medium cursor-pointer hover:underline underline-offset-4">
+                        Profile
+                    </a>
+                </div>
+            @else
+                <div class="flex gap-2 items-center">
+                    <a href="{{ route('login') }}"
+                        class="font-medium cursor-pointer hover:underline underline-offset-4">
+                        Login
+                    </a>
+                    <a href="{{ route('register') }}"
+                        class="font-medium cursor-pointer hover:underline underline-offset-4">
+                        Register
+                    </a>
+                </div>
+            @endauth
         </div>
-        @auth
-            <div class="flex items-center gap-2">
-                <a href="{{ route('profile') }}"
-                    class="text-sm font-light ">Profile</a>
-                <form action="{{ route('logout') }}" method="POST" class="">
-                    @csrf
-
-                    <button type="submit"
-                        class="text-sm font-light cursor-pointer">
-                        logout
-                    </button>
-                </form>
-            </div>
-        @else
-            <div class="flex gap-2 ">
-                <a href="{{ route('login') }}" class="text-sm font-light ">Login</a>
-                <a href="{{ route('register') }}"
-                    class="text-sm font-light ">Register</a>
-            </div>
-        @endauth
     </nav>
 </header>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,7 +1,7 @@
 <x-user-layout title="Home">
     <div class="container px-4 py-2 mx-auto">
-        <h1 class="font-bold text-red-500 text-7xl">
-            Ini adalah home page
-        </h1>
+        <section>
+
+        </section>
     </div>
 </x-user-layout>
